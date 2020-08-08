335bec0bbdd37839711d0c85a32fc4d369b28dfa
Neven Klofutar <neven.klofutar@gmail.com>
2020-08-08 23:01:24 +0200

    ExceptionMiddlewareExtension returns 500 ProblemDetailException

--- Web.Api/Extensions/ExceptionMiddlewareExtensions.cs
+++ Web.Api/Extensions/ExceptionMiddlewareExtensions.cs
@@ -18,22 +18,22 @@ namespace Web.Api.Extensions
         {
             app.UseExceptionHandler(appError =>
             {
-                appError.Run(async context =>
-                {
-                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                    context.Response.ContentType = "application/json";
-                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
-                    if (contextFeature != null)
-                    {
-                        logger.LogError($"Something went wrong: {contextFeature.Error}");
-                        //await context.Response.WriteAsync(new ErrorDetails()
-                        //{
-                        //    StatusCode = context.Response.StatusCode,
-                        //    Message = "Internal Server Error."
-                        //}.ToString());
-                        throw new ProblemDetailsException(500, "Internal Server Error.");
-                    }
-                });
+                //appError.Run(async context =>
+                //{
+                //    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                //    context.Response.ContentType = "application/json";
+                //    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                //    if (contextFeature != null)
+                //    {
+                //        logger.LogError($"Something went wrong: {contextFeature.Error}");
+                //        await context.Response.WriteAsync(new ErrorDetails() {
+                //            StatusCode = context.Response.StatusCode,
+                //            Message = "Internal Server Error."
+                //        }.ToString());
+                //    }
+                //});
+
+                throw new ProblemDetailsException(500, "Internal Server Error.");
             });
         }
     }
